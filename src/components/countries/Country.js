3eb24db2446347b820990ebfbcd16c6cb1554319
WallisonStorck <storck779@gmail.com>
2022-03-31 16:30:52 -0400

    Converting Class Components to Functional Components

--- src/components/countries/Country.js
+++ src/components/countries/Country.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import css from "./countries.module.css";
 
-export default class Country extends Component {
-  render() {
-    const { country } = this.props;
+export default function Country(props) {
+  const { country } = props;
 
-    return (
-      <div className={css.country}>
-        <img className={css.flagCountry} src={country.flag} alt="Flag" />
-        <span className={css.nameCountry}>{country.name}</span>
-      </div>
-    );
-  }
+  return (
+    <div className={css.country}>
+      <img className={css.flagCountry} src={country.flag} alt="Flag" />
+      <span className={css.nameCountry}>{country.name}</span>
+    </div>
+  );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,37 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import { formatNumber } from "../helpers/formatHelper";
 import css from "./header.module.css";
 
-export default class Header extends Component {
-  handleChangeInput = (event) => {
-    const { onChangeFilter } = this.props;
+export default function Header(props) {
+  const handleChangeInput = (event) => {
+    const { onChangeFilter } = props;
     const newText = event.target.value;
     onChangeFilter(newText);
   };
 
-  render() {
-    const { filter, countryCount, populationCount } = this.props;
-    /**
-     * Input recebe a prop filter, que é o valor atual do input e o "onChange"
-     * ou seja, a qualquer alteração do input, chame "handleChangeInput", a
-     * função que encapsula o método "onChangeFilter" recebido via props.
-     */
-    return (
-      <div className={`${css.flexRow} ${css.border}`}>
-        <input
-          className={css.inputCountries}
-          type="text"
-          value={filter}
-          placeholder="Filter"
-          onChange={this.handleChangeInput}
-        />
-        <span>
-          Countries: <strong>{countryCount}</strong> |
-        </span>
-        <span>
-          Population: <strong>{formatNumber(populationCount)}</strong> |
-        </span>
-      </div>
-    );
-  }
+  const { filter, countryCount, populationCount } = props;
+  /**
+   * Input recebe a prop filter, que é o valor atual do input e o "onChange"
+   * ou seja, a qualquer alteração do input, chame "handleChangeInput", a
+   * função que encapsula o método "onChangeFilter" recebido via props.
+   */
+  return (
+    <div className={`${css.flexRow} ${css.border}`}>
+      <input
+        className={css.inputCountries}
+        type="text"
+        value={filter}
+        placeholder="Filter"
+        onChange={handleChangeInput}
+      />
+      <span>
+        Countries: <strong>{countryCount}</strong> |
+      </span>
+      <span>
+        Population: <strong>{formatNumber(populationCount)}</strong> |
+      </span>
+    </div>
+  );
 }
